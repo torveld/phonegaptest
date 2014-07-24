8769caaf5f65ec829b2c9071691e9e2330d377dd
tor.veld <tor.veld@bouvet.no>
2014-07-24 20:06:54 +0200

    dfs

--- javaskript.js
+++ javaskript.js
@@ -18,6 +18,7 @@ function getStorage() {
     var item = localStorage.getItem("number");
 	
 	if(item != null) {
+		//self.registerEvents();
 		self.showAlert('Number gathered from local storage', 'info');
 		document.getElementById("number").innerHTML = item;
 	} else {
@@ -30,5 +31,27 @@ function showAlert(message, title) {
         navigator.notification.alert(message, null, title, 'OK');
     } else {
         alert(title ? (title + ": " + message) : message);
+    }
+}
+
+function registerEvents() {
+    var self = this;
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
     }
 }
