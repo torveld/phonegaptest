9f17ab1c97b5581c11d3f30649c1f083150a653c
tor.veld <tor.veld@bouvet.no>
2014-07-24 08:51:00 +0200

    naa da

--- javaskript.js
+++ javaskript.js
@@ -14,10 +14,21 @@ function subtract(amount) {
 }
 
 function getStorage() {
+	var self = this;
     var item = localStorage.getItem("number");
 	
 	if(item != null) {
+		self.showAlert('Number gathered from local storage', 'info');
 		document.getElementById("number").innerHTML = item;
+	} else {
+		self.showAlert('No number in local storage, start at 0', 'info');
 	}
 }
 
+function showAlert(message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+}
